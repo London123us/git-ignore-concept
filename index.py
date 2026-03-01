8064933617a06c0e9cab02fb9886dc3b902ab24e
London123us <neerajp898@gmail.com>
2026-03-02 02:07:41 +0530

    Done

--- index.py
+++ index.py
@@ -3,3 +3,5 @@ print("Iron Man Died")
 print("Learning Git & Github are fun")
 print("Done2")
 print("done 4")
+
+print("Already done for gitignore.")
---
+++ js.py
@@ -0,0 +1,2 @@
+print("Its Dr. doom files")
+print("Rest other's files are ignored by git using gitignore")
